4ee05c1df7363a9a8a60a156bbb44a61e5f6dc94
catcherxiao <xcg3330@163.com>
2024-12-22 13:36:24 +0800

    Initial Commit

---
+++ BaziAnalysis/BaziAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaziAnalysisApp.swift
+//  BaziAnalysis
+//
+//  Created by catcher on 2024/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BaziAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
